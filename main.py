4205ad97c9284b8abbdfcbc93b4422314c039857
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-11-15 23:25:34 -0500

    Inicio con restricciones calidad

---
+++ main.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+import pyomo.environ as pyo
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+N = 3 # i
+M = 3 # j
+Pf = 3
+
+d = {(1, 1): 1.7, (1, 2): 7.2, (1, 3): 9.0, (1, 4): 8.3,
+(2, 1): 2.9, (2, 2): 6.3, (2, 3): 9.8, (2, 4): 0.7,
+(3, 1): 4.5, (3, 2): 4.8, (3, 3): 4.2, (3, 4): 9.3}
+
+model = pyo.ConcreteModel()
+model.i = RangeSet(N)   #producto intermedio
+model.j = RangeSet(M)   #producto final
+
+
+model.RbnPf = Param(model.i, initialize={1: 350,2: 600 ,3: 400}, doc='Capacity of plant i in cases')
+model.Rbn = Param(model.i, initialize={1: 350,2: 600 ,3: 400}, doc='Research Blending number producto i ')
+
+
+model.x = Var( model.i, model.j, bounds=(0,None))
+
+def supply_rule(model, j):
+  return sum(model.Rbn[i]*model.x[i,j] for i in model.i) <= model.RbnPf[j]
+
+model.supply = Constraint(model.j, rule=supply_rule, doc='Restriccion de calidad producto final j')
+
+print(model.pprint())
