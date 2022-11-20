f383e83921b97ce25fcad53918674ba5901e3671
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-11-20 15:39:30 -0500

    arreglan paramatetros

--- main.py
+++ main.py
@@ -9,42 +9,17 @@ model  =  pyo.ConcreteModel()
 
 model.i = Set(initialize=['Nvl','Np','Ref'], doc='Productos Intermedios')
 model.j = Set(initialize=['G83', 'G90', 'G94'], doc='Productos Finales')
-#REsultado Winqsb de pinon comprobando
-#G83
-tranmezclag83=55.48230*159.4096 + 52.52652*26.4804+ 65.13049*114.11
-mezcla83result = 300* 58.8912500199999
-print(round(tranmezclag83,2))
-print(round(mezcla83result,2))
-#G90
-tranmezclag90=55.48230*225.412 + 65.13049*559.6451
-mezcla90result = 785.0571 * 62.3602269999997
-print(round(tranmezclag90,1))
-print(round(mezcla90result,1))
-#rvp
-#G83
-tranmezclag83=0.61140*159.4096 + 0.03713*26.4804+ 0.02998*114.11
-mezcla83result = 300* 0.617498832595756
-print(round(tranmezclag83,2))
-print(round(mezcla83result,2))
-
-#azufre
-#G83
-tranmezclag83=0.61140*159.4096 + 0.03713*26.4804+ 0.02998*114.11
-mezcla83result = 300* 0.617498832595756
-print(round(tranmezclag83,2))
-print(round(mezcla83result,2))
-#---------------------------
 
 #PARAMETROS
 
 productosIntermedios = {
     'Nvl': {'Rendimiento': 0.04776, 'RBN': 55.48230, 'RVP': 0.61140, 'PAzufre': 64.72995, 'Densidad': 0.66703},
     'Np':  {'Rendimiento': 0.151957, 'RBN': 52.52652, 'RVP': 0.03713, 'PAzufre': 345.32027, 'Densidad': 0.74964},
-    'Ref': {'Rendimiento': 0.82455, 'RBN': 65.13, 'RVP': 0.02998, 'PAzufre': 64.72995, 'Densidad': 0.78712}
+    'Ref': {'Rendimiento': 0.82455, 'RBN': 65.13, 'RVP': 0.02998, 'PAzufre': 0.78492, 'Densidad': 0.78712}
 }
 productosFinales = {
     'G83' : {'price': 3300, 'RBNmin': 58.89, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
-    'G90' : {'price': 3500, 'RBNmin': 61.36, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
+    'G90' : {'price': 3500, 'RBNmin': 62.36, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
     'G94' : {'price': 3746, 'RBNmin': 65.13, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200}
 }
 
@@ -112,11 +87,11 @@ model.obj = Objective(expr = obj_rule, sense = maximize,doc = 'funcion objetivo'
 
 # resolvemos el problema e imprimimos resultados
 def pyomo_postprocess(options=None, instance=None, results=None):
-    gx.display()
+    x.display()
 
 # utilizamos solver glpk
 opt = SolverFactory("glpk")
-resultados = opt.solve(model)
+resultados = opt.solve(model, tee = True)
 
 # imprimimos resultados
 print("\nSolución óptima encontrada\n" + '-'*80)
