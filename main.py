b677d7d6aee35f6bc9962884cebc7ca0a0e72df4
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-11-19 23:39:51 -0500

    arregaldo con resultado

--- main.py
+++ main.py
@@ -43,38 +43,19 @@ productosIntermedios = {
     'Ref': {'Rendimiento': 0.82455, 'RBN': 65.13, 'RVP': 0.02998, 'PAzufre': 64.72995, 'Densidad': 0.78712}
 }
 productosFinales = {
-    'G83' : {'RBNmin': 58.8912500199999, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
-    'G90' : {'RBNmin': 61.3602269999997, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
-    'G94' : {'RBNmin': 65.1304929200004, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200}
+    'G83' : {'price': 3300, 'RBNmin': 58.89, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
+    'G90' : {'price': 3500, 'RBNmin': 61.36, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200},
+    'G94' : {'price': 3746, 'RBNmin': 65.13, 'RVPmax': 0.617498832595756, 'Azufemax':1000, 'Densidadmin': 0.7200}
 }
-precioPF = {
-  'G83': { 'price': 3300},
-    'G90': {'price': 3500},
-    'G94': {'price': 3746}
-}
+
 demandaPF = {
   'G83': { 'Min':0 , 'Max':'M'},
-    'G90': {'Min':750, 'Max':'M'},
-    'G94': {'Min':400, 'Max':'M'}
+    'G90': {'Min':0, 'Max':'M'},
+    'G94': {'Min':0, 'Max':'M'}
 }
 # display feed information
-print(pd.DataFrame.from_dict(productosFinales).T)
-
-model.Rbn = Param(model.i, initialize = {'Nvl': 55.48230,'Np': 52.52652, 'Ref': 65.13}, doc = 'Research Blending number producto i')
-model.Ipvr = Param(model.i, initialize = {'Nvl': 0.61140, 'Np': 0.03713 ,'Ref': 0.02998}, doc = 'IPVR del producto i ')
-model.PAzufre = Param(model.i, initialize = {'Nvl': 64.72995, 'Np': 345.32027 ,'Ref': 0.78492}, doc = 'azufre que es calculador por el P = d*v del producto i ')
-model.densidad = Param( model.i, initialize = {'Nvl': 0.66703, 'Np':0.74964, 'Ref':0.78712}, doc = 'Densidad Nvl, Nvp, Ar')
-
-model.RbnPf = Param(model.j, initialize = {'G83': 58.8912500199999, 'G90': 61.3602269999997, 'G94': 65.1304929200004 }, doc = 'Limite de Research Blending Number producto final j')
-model.IpvrF = Param(model.j, initialize = {'G83': 0.617498832595756 ,'G90': 0.617498832595756 ,'G94': 0.617498832595756}, doc = 'Limite de indice RVP producto final j')
-model.PAzufreF = Param(model.j, initialize = {'G83': 1000, 'G90': 1000 ,'G94': 1000}, doc = 'Limite del azufre  producto final j')
-model.PdensidadF = Param( model.j, initialize = {'G83': 0.7200, 'G90':0.7200, 'G94':0.7200}, doc = 'Densidad producto j')
-model.preciog = Param( model.j, initialize = {'G83': 3300, 'G90':3500, 'G94':3746}, doc = 'Precio Gaso83, Gaso90, Gaso94')
-
-
-model.demandag = Param( model.j, initialize = {'G83': 0, 'G90':750, 'G94':400}, doc = 'Demanda Gaso83, Gaso90, Gaso94')
+#print(pd.DataFrame.from_dict(productosFinales).T)
 model.Destil = Param(initialize = 8744, doc = 'Destilacion atmosfereica')
-model.rendimiento = Param( model.i, initialize = {'Nvl': 0.04776, 'Np':0.151957, 'Ref':0.82455}, doc = 'Rendimientos Nvl, Nvp, Ar')
 
 #VARAIABLES
 model.Ar = Var(within = NonNegativeReals, bounds = (0, 1526), doc = 'Alimentacion al reformador')
@@ -85,51 +66,50 @@ model.x = Var( model.i, model.j,within = NonNegativeReals, bounds = (0, None), d
 x = model.x
 model.gx = Var(model.j,within = NonNegativeReals, doc = 'gasolina j')
 gx = model.gx
-#RESTRICCINES
+
+#RESTRICCIONES
 
 #Balance de materiales(Lo que se extrae de los productos intermedios no sobrepasara la existencia)
-model.NvlMB = Constraint(expr = x['Nvl','G83'] + x['Nvl','G90'] + x['Nvl','G94'] <= model.rendimiento['Nvl']*model.Destil, doc = 'Balance de Materiales para la Nafta Virgen ligera')
+model.NvlMB = Constraint(expr = x['Nvl','G83'] + x['Nvl','G90'] + x['Nvl','G94'] <= productosIntermedios['Nvl']['Rendimiento']*model.Destil, doc = 'Balance de Materiales para la Nafta Virgen ligera')
 
-model.NvpMB = Constraint(expr = x['Np','G83'] + x['Np','G90'] + x['Np','G94'] + model.Ar <= model.rendimiento['Np']*model.Destil, doc = 'Balance de Materiales para la Nafta Virgen Pesada')
+model.NvpMB = Constraint(expr = x['Np','G83'] + x['Np','G90'] + x['Np','G94'] + Ar <= productosIntermedios['Np']['Rendimiento']*model.Destil, doc = 'Balance de Materiales para la Nafta Virgen Pesada')
 
-model.Ref = Constraint(expr = x['Ref','G83'] + x['Ref','G90'] + x['Ref','G94'] <= model.rendimiento['Ref']*Ar, doc = 'Balance de Materiales para el Refromado')
+model.Ref = Constraint(expr = x['Ref','G83'] + x['Ref','G90'] + x['Ref','G94'] <= productosIntermedios['Ref']['Rendimiento']*Ar, doc = 'Balance de Materiales para el Refromado')
 #La suma de todas las partes debe ser el total de la mezcla a elaborar
 def wgasolina_rule(model, j):
-  return  sum(model.densidad[i]*x[i,j] for i in model.i) == Wg[j] 
-  
+  return  sum(productosIntermedios[i]['Densidad']*x[i,j] for i in model.i) == Wg[j]   
 model.wgasolina = Constraint(model.j, rule = wgasolina_rule, doc = 'El peso en masa de todos los productos intermedios tiene que ser mayor o igual que el peso en masa del producto final j')
+
+#gasolina j
+model.gasolinaj = ConstraintList()
+for j in model.j:
+    model.gasolinaj.add(sum(x[i,j] for i in model.i) == gx[j])
+
 
 #calidad
 
+model.calidad = ConstraintList()
+for j in model.j:
+    model.calidad.add(sum(x[i,j]*productosIntermedios[i]['RBN'] for i in model.i) -  productosFinales[j]['RBNmin']*gx[j] >= 0)
+    model.calidad.add(sum(x[i,j]* productosIntermedios[i]['Densidad']  for i in model.i) -  productosFinales[j]['Densidadmin']*gx[j] >= 0)
+    model.calidad.add(sum(x[i,j]*productosIntermedios[i]['RVP'] for i in model.i) - productosFinales[j]['RVPmax']*gx[j] <= 0)
+    model.calidad.add(sum(x[i,j]*productosIntermedios[i]['PAzufre']   for i in model.i) -  productosFinales[j]['Azufemax']*gx[j]  <= 0)
 
-def rbn_rule(model, j):
-  return sum(model.Rbn[i]*x[i,j] for i in model.i) >= model.RbnPf[j] * gx[j]
-  
-model.rbn = Constraint(model.j, rule = rbn_rule, doc = 'Restriccion RON de calidad producto final j')
-
-def ipvr_rule(model, j):
-  return sum(model.Ipvr[i]*x[i,j] for i in model.i) >= model.IpvrF[j] * gx[j]
-  
-model.ipvr = Constraint(model.j, rule = ipvr_rule, doc = 'Restriccion RVP de calidad producto final j')
-#
-def pazufre_rule(model, j):
-  return   sum(model.PAzufre[i]*x[i,j] for i in model.i) >= model.PAzufreF[j] * gx[j]
-model.azufre = Constraint(model.j, rule = pazufre_rule, doc = 'Restriccion Peso Azufre de calidad producto final j')
-
-
-#gasolina j
-model.g83 = Constraint(expr = x['Nvl','G83'] + x['Np','G83'] + x['Ref','G83'] == gx['G83'], doc = 'Gasolina 83')
-model.g90 = Constraint(expr = x['Nvl','G90'] + x['Np','G90'] + x['Ref','G90'] == gx['G90'], doc = 'Gasolina 90')
-model.g94 = Constraint(expr = x['Nvl','G94'] + x['Np','G90'] + x['Ref','G94'] == gx['G94'], doc = 'Gasolina 94')
 
 #demanda
-def demanda_rule (model, j) :
-  return  model.gx[j] >= model.demandag[j]
-model.demanda = Constraint(model.j,  rule = demanda_rule, doc = 'Restriccion para las demandas')
+model.demanda = ConstraintList()
+for j in model.j:
+  if isinstance(demandaPF[j]['Min'], (int, float)):
+    model.demanda.add(model.gx[j] >= demandaPF[j]['Min'])
+  if isinstance(demandaPF[j]['Max'], (int, float)):
+    model.demanda.add(model.gx[j] <= demandaPF[j]['Max'])  
+
 model.demanda.pprint()
+
+
 #FUNCION OBJETIVO
 def obj_rule(model):
-  return sum( model.preciog[j]*gx[j] for j in model.j )
+  return sum( productosFinales[j]['price']*gx[j] for j in model.j )
 model.obj = Objective(expr = obj_rule, sense = maximize,doc = 'funcion objetivo')
 
 # resolvemos el problema e imprimimos resultados
@@ -143,7 +123,6 @@ resultados = opt.solve(model)
 # imprimimos resultados
 print("\nSolución óptima encontrada\n" + '-'*80)
 pyomo_postprocess(None, None, resultados)
-#instance.load(resultados) # Loading solution into results object
 if (resultados.solver.status == SolverStatus.ok) and (resultados.solver.termination_condition == TerminationCondition.optimal):
   print(' Do something when the solution in optimal and feasible')
 elif (resultados.solver.termination_condition == TerminationCondition.infeasible):
